9ddc3f5d6bbde8179e2f77a4434e3cf70ce8fa3c
LKH <airforce940706@gmail.com>
2025-05-29 16:40:54 +0900

    [이건호] movie / theater DAO / DTO

---
+++ src/main/java/data/dto/TheaterDTO.java
@@ -0,0 +1,36 @@
+package data.dto;
+
+public class TheaterDTO {
+	
+	private String id;
+	private String region;
+	private String name;
+	private String address;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+}
