633f168d055412eb2511cceb58767f0067b029ce
hjyoo <hjyoo330@gmail.com>
2025-05-29 17:54:24 +0900

    [유혜지]회원가입폼,userDao,Dto 작성

---
+++ src/main/java/data/dto/UserDTO.java
@@ -0,0 +1,89 @@
+package data.dto;
+
+import java.sql.Timestamp;
+
+public class UserDTO {
+
+	private String id;
+	private String username;
+	private String password;
+	private String name;
+	private String gender;
+	private int age;
+	private String phone;
+	private String address;
+	private int mileage;
+	private String user_type;
+	private Timestamp signup_at;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getMileage() {
+		return mileage;
+	}
+	public void setMileage(int mileage) {
+		this.mileage = mileage;
+	}
+	public String getUser_type() {
+		return user_type;
+	}
+	public void setUser_type(String user_type) {
+		this.user_type = user_type;
+	}
+	public Timestamp getSignup_at() {
+		return signup_at;
+	}
+	public void setSignup_at(Timestamp signup_at) {
+		this.signup_at = signup_at;
+	}
+	
+	
+	
+}
