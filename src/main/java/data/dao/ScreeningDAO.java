a0e541aaf2aa316b05441f299515ddf9b0ca8587
ssosio <cksl1213@naver.com>
2025-06-04 14:08:19 +0900

    중간merge

--- src/main/java/data/dao/ScreeningDAO.java
+++ src/main/java/data/dao/ScreeningDAO.java
@@ -22,50 +22,7 @@ public class ScreeningDAO {
 		return dao;
 	}
 
-	//movie_id로 DTO반환
-	public List<HashMap<String, String>> getScreeningDatas(String movie_id){
-		List<HashMap<String, String>> list = new ArrayList<HashMap<String,String>>();
-		Connection conn = db.getConnection();
-		PreparedStatement pst = null;
-		ResultSet rs = null;
-		String sql ="select t.region, t.name as theater_name , a.name as auditorium_name , s.start_time,\r\n"
-						+ "(select count(*) from seat st where a.id = st.auditorium_id ) AS total_seat,\r\n"
-						+ "(select count(*) \r\n"
-						+ "from seat_reserved str \r\n"
-						+ "join reservation rs on str.reservation_id = rs.id \r\n"
-						+ "where rs.screening_id = s.id) AS reserved_seats\r\n"
-						+ "from screening s\r\n"
-						+ "join auditorium a on a.id = s.auditorium_id\r\n"
-						+ "join movie m on s.movie_id = m.id\r\n"
-						+ "join theater t on t.id = a.theater_id\r\n"
-						+ "where s.movie_id =?";
-
-		try {
-			pst = conn.prepareStatement(sql);
-			pst.setString(1, movie_id);
-			rs = pst.executeQuery();
-
-			while(rs.next()) {
-
-				HashMap<String, String> map = new HashMap<String, String>();
-				map.put("region", rs.getString("region"));
-				map.put("theater_name", rs.getString("theater_name"));
-				map.put("auditorium_name", rs.getString("auditorium_name"));
-				map.put("start_time", rs.getString("start_time"));
-				map.put("total_seat", rs.getString("total_seat"));
-				map.put("reserved_seats", rs.getString("reserved_seats"));
-
-				list.add(map);
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-			db.dbClose(rs, pst, conn);
-		}
-		return list;
-	};
-
+	//극장갯수
 	public List<HashMap<String, String>> getTheaterCount(String movie_id){
 		List<HashMap<String, String>> list = new ArrayList<HashMap<String,String>>();
 		Connection conn = db.getConnection();
@@ -109,13 +66,15 @@ public class ScreeningDAO {
 		ResultSet rs = null;
 		String sql = "select\r\n"
 				+ " s.start_time, \r\n"
+				+ "s.id as screening_id, \r\n"
+				+ "m.poster_url as poster, \r\n"
 				+ " m.title,\r\n"
 				+ " a.name as auditorium_name,\r\n"
 				+ " m.runtime,\r\n"
 				+ " t.name as theater_name,\r\n"
 				+ " count(DISTINCT seat.id) as total_seat,\r\n"
 				+ " count(DISTINCT r.id) as reserved_seatcount,\r\n"
-				+ " count(distinct seat.id) - count(DISTINCT sr.id) as remaining_seats\r\n"
+				+ " count(distinct seat.id) - count(DISTINCT sr.id) as remaining_seat\r\n"
 				+ "from screening s\r\n"
 				+ "join movie m on s.movie_id = m.id\r\n"
 				+ "join auditorium a on s.auditorium_id = a.id\r\n"
@@ -124,7 +83,7 @@ public class ScreeningDAO {
 				+ "left join reservation r on r.screening_id = s.id\r\n"
 				+ "left join seat_reserved sr on sr.reservation_id = r.id\r\n"
 				+ "where s.movie_id =? and t.name = ?\r\n"
-				+ "group by s.id";
+				+ "group by s.id  order by s.start_time";
 
 		try {
 			pst = conn.prepareStatement(sql);
@@ -135,12 +94,14 @@ public class ScreeningDAO {
 				HashMap<String, String> map = new HashMap<String, String>();
 
 				map.put("start_time",rs.getString("start_time"));
+				map.put("screening_id",rs.getString("screening_id"));
+				map.put("poster",rs.getString("poster"));
 				map.put("title",rs.getString("title"));
 				map.put("auditorium_name",rs.getString("auditorium_name"));
 				map.put("runtime",rs.getString("runtime"));
 				map.put("theater_name",rs.getString("theater_name"));
 				map.put("total_seat",rs.getString("total_seat"));
-				map.put("reserved_seatcount",rs.getString(" reserved_seatcount"));
+				map.put("reserved_seatcount",rs.getString("reserved_seatcount"));
 				map.put("remaining_seat",rs.getString("remaining_seat"));
 
 				list.add(map);
--- src/main/webapp/book/booking/showTimes.jsp
+++ src/main/webapp/book/booking/showTimes.jsp
@@ -1,8 +1,38 @@
+<%@page import="org.json.simple.JSONObject"%>
+<%@page import="org.json.simple.JSONArray"%>
+<%@page import="data.dao.ScreeningDAO"%>
+<%@page import="java.util.HashMap"%>
+<%@page import="java.util.List"%>
+<%@page import="data.dao.TheaterDAO"%>
 <%@ page language="java" contentType="application/json; charset=UTF-8"
     pageEncoding="UTF-8"%>
     <%
     String movie_id = request.getParameter("movie_id");
     String theaterName = request.getParameter("theaterName");
+	System.out.print("movie_id: "+movie_id);
+	System.out.print("theaterName: "+theaterName);
 
+	ScreeningDAO dao = ScreeningDAO.getInstance();
 
+	List<HashMap<String,String>> list = dao.getTheaterScreeningInfo(movie_id, theaterName);
+
+	JSONArray arr = new JSONArray();
+
+	for(HashMap<String,String>map: list){
+		JSONObject ob = new JSONObject();
+
+		ob.put("start_time", map.get("start_time"));
+		ob.put("screening_id", map.get("screening_id"));
+		ob.put("poster", map.get("poster"));
+		ob.put("title", map.get("title"));
+		ob.put("auditorium_name", map.get("auditorium_name"));
+		ob.put("runtime", map.get("runtime"));
+		ob.put("theater_name", map.get("theater_name"));
+		ob.put("total_seat", map.get("total_seat"));
+		ob.put("reserved_seatcount", map.get("reserved_seatcount"));
+		ob.put("remaining_seat", map.get("remaining_seat"));
+
+		arr.add(ob);
+	}
     %>
+    <%=arr.toString()%>
